1c744fae58bf4016cee1aeb268210f60cef9c4d8
BensonZhengFromACCI <343804126@tdsb.ca>
2026-03-29 05:03:50 +0000

    so easy mr kalizs

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,34 @@ public class PracticeProblem {
 
 	}
 
+public static String evenOrOdd(int number) {
+    if (number % 2 == 0) {
+        return "Even";
+    }
+    if (number % 2 != 0) {
+        return "Odd";
+    }
+    return "";
 }
+
+public static String teacherOrStudent(String name) {
+    if (name == "Kalisz") {
+        return "Teacher";
+    }
+    if (name != "Kalisz") {
+        return "Student";
+    }
+    return "";
+}
+
+public static int fartherFromZero(int number) {
+    if (number > 0) {
+        return number + 5;
+    }
+    if (number < 0) {
+        return number - 5;
+    }
+    return 0;
+}
+}
+
